cf2b5006aad38108c8c736c918a157fe79fbcca1
Zanfir Liliana <117457342+Liliana24znf@users.noreply.github.com>
2025-04-08 23:19:41 +0300

    Delete Step

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,6 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import TimetableLayout from "./components/TimetableLayout";
-import TimetableUniversityStep from "./pages/TimetableUniversityStep";
-import TimetableGroupsStep from "./pages/TimetableGroupsStep";
-import TimetableGroupingsStep from "./pages/TimetableGroupingsStep.jsx";
-import TimetableActivitiesStep from "./pages/TimetableActivitiesStep";
-import TimetableTeachersStep from "./pages/TimetableTeachersStep";
-import TimetableCoursesStep from "./pages/TimetableCoursesStep";
-import TimetableRulesStep from "./pages/TimetableRulesStep";
 import GeneratedTimetable from "./pages/GeneratedTimetable";
 import Profesori from "./pages/Profesori.jsx";
 import Sali from "./pages/Sali.jsx";
@@ -28,15 +20,7 @@ function App() {
         <Route path="/profesori" element={<Profesori />} />
         <Route path="/orar-generat" element={<GeneratedTimetable />} />
         <Route path="/sali" element={<Sali />} />
-        <Route path="/timetable" element={<TimetableLayout />}>
-          <Route path="universitate" element={<TimetableUniversityStep />} />
-          <Route path="grupe" element={<TimetableGroupsStep />} />
-          <Route path="grupari" element={<TimetableGroupingsStep />} />
-          <Route path="activitati" element={<TimetableActivitiesStep />} />
-          <Route path="profesori" element={<TimetableTeachersStep />} />
-          <Route path="cursuri" element={<TimetableCoursesStep />} />
-          <Route path="reguli" element={<TimetableRulesStep />} />
-        </Route>
+       
       </Routes>
 
     </Router>
